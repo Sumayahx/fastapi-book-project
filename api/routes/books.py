243de105be09c0e86615721cd46d475c178aa470
Sumayahx <sumayashittu@gmail.com>
2025-02-11 10:56:12 +0100

    initial commit

--- api/routes/books.py
+++ api/routes/books.py
@@ -40,13 +40,20 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
-
 @router.get(
     "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+@router.get(
+        "/api/v1/books/{book_id}", response_model= Book, status_code=status.HTTP_200_OK
+)
+async def get_book(book_id: int):
+    book = db.get_book(book_id)
+    if not book:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    return book
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
@@ -54,7 +61,6 @@ async def update_book(book_id: int, book: Book) -> Book:
         status_code=status.HTTP_200_OK,
         content=db.update_book(book_id, book).model_dump(),
     )
-
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
